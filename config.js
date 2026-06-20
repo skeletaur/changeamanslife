248e24f2cbd79e389034dc2fa2234f9278a8f2dc
skeletaur <contact@changeamanslife.com>
2026-06-20 02:09:09 -0400

    Update GoFundMe link to new fundraiser

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Update these values as needed.
 
 window.SITE_CONFIG = {
-  donateUrl: "https://www.gofundme.com/f/room-to-breathe-for-my-family",
+  donateUrl: "https://www.gofundme.com/f/2xq35t-room-to-breathe-for-my-family",
   contactEmail: "contact@changeamanslife.com",
 };
